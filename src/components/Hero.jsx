552ad65a477f0517bc57924de35893be599c869f
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-22 12:24:01 +0200

    Feat: Add the Heading in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,18 @@ import { ComputersCanvas } from './canvas'
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-
+      <div className={`${styles.paddingX} absolute top-[154px] inset-0 mx-w-7xl flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I am <span className='text-[#915eff]'>Ezechiel</span></h1>
+          <p className={`${styles.heroSubText} text-white-100 mt-2`}>
+            I develop user interface, data visualization and web applications.
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
