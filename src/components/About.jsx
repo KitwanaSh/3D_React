0951c4c6a8e2adb7d3f31d951b556b79ac2f2075
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-23 11:43:48 +0200

    Feat: add service-title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
-import Tilt from 'react-tilt'
+// import Tilt from 'react-tilt'
 import { motion } from 'framer-motion'
 
 import { styles } from '../styles'
 import { services } from  '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+const ServiceCard = ({ index, title, icon}) => {
+  return (
+    <p>{title}</p>
+  )
+}
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p 
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 max-w-3xl text-secondary text-[17px] leading-[30px]'
+      >
+        I am a skilled frontend developer with expertise in JavaScript and React.
+        I am also a data analyst with expertise in python, Tableau, SQL as tools to use and communication
+        skills, creative thinking, problem solving and developing. Let's work together to bring you ideas
+        to life.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
   )
 }
 
