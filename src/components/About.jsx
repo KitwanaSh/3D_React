3619227a697a888259b2a4a609ad967cbbbe4222
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-23 12:22:41 +0200

    Fix: Update the Titl react

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Tilt from 'react-tilt'
+import { Tilt } from 'react-tilt'
 import { motion } from 'framer-motion'
 
 import { styles } from '../styles'
@@ -8,7 +8,25 @@ import { fadeIn, textVariant } from '../utils/motion'
 
 const ServiceCard = ({ index, title, icon}) => {
   return (
-    <p>{title}</p>
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div 
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt = {title} 
+          className='w-16 h-16 object-contain'/>
+          <h3 className='text-white text-[20px] text-center font-bold'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
