1350016d6db9595a9674d461a06f291e7a52693d
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-25 12:14:25 +0200

    Fix: Update the Tilt in Works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import Tilt from "react-tilt"
+import {Tilt} from "react-tilt"
 import{ motion } from "framer-motion"
 
 import { styles } from "../styles"
@@ -7,10 +7,54 @@ import { SectionWrapper } from "../hoc"
 import { projects } from "../constants"
 import { fadeIn, textVariant } from "../utils/motion"
 
+const ProjectCard = ({index, name, description, tags, image, source_code_link }) => {
+  return (
+    <motion.div 
+      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+    >
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+
+      </Tilt>
+    </motion.div>
+  )
+}
+
 const Works = () => {
   return (
     <>
-    
+      <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>My Work</p>
+      <h2 className={styles.sectionHeadText}>Projects.</h2>
+    </motion.div>
+    <div className="w-full flex">
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Following projects showcases my skills and experience through real-world examples of my work.
+        Each project is briefly described with links to code repositories and live demos in it.
+        It reflects my ability to solve comple problems, working with different technologies, and
+        manage projects effectively.
+      </motion.p>
+    </div>
+
+    <div className="mt-20 flex flex-wrap gap-7">
+      {projects.map((project, index) => (
+        <ProjectCard 
+          key={`project-${index}`}
+          index={index}
+          {...project}
+        />
+      ))}
+
+    </div>
     </>
   )
 }
