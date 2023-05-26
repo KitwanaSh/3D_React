763d12e2da22b221eb8de8472985fb7bdcbb625c
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-26 10:24:37 +0200

    Feat: Add the headings in Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,8 +16,18 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false)
 
+  const handleChange = (e) => {}
+  const handleSubmit = (e) => {}
+
   return (
-    <div>Contact</div>
+    <div className='xl:mt-12 xl:flex-row flex flex-col-reverse gap-10 overflow-hidden'>
+      <motion className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+        variants={slideIn("left", "tween", 0.2, 1)}
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion>
+    </div>
   )
 }
 
