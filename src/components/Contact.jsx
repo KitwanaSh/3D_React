7d7dab2dbbb719d39e7f381ea4df4b0d06f92aad
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-26 10:51:20 +0200

    Feat: Layout the button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -66,6 +66,9 @@ const Contact = () => {
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
             />
           </label>
+          <button type="submit"
+            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
+          >{loading ? "Sending..." : "Send"}</button>
 
         </form>
       </motion>
