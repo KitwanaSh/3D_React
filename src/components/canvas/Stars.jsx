3bc0acee394e82dd91356d3dc48a2c018c133385
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-26 11:42:05 +0200

    Feat: Start the starscanvas'

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useState, useRef, Suspense } from 'react'
+import { Canvas, useFrame} from '@react-three/fiber'
+import { Points, PointMaterial, Preload } from '@react-three/drei'
+import * as random from 'maath/random/dist/maath-random.esm'
 
-const Stars = () => {
+const Stars = (props) => {
   return (
     <div>Stars</div>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default StarsCanvas
