b24676f0c3c97bb9eb2650d3423ce1d679feb74d
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-26 11:34:27 +0200

    Feat: Done with the earth model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -20,9 +20,14 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameLoop='demand'
+      frameloop='demand'
       gl={{ preserveDrawingBuffer: true }}
-      camera={{ }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
