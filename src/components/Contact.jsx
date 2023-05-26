76ac0024d84f9ba66de3fcd7e31a2c014c55b3d2
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-26 10:02:56 +0200

    Feat: Start the Contanct section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useState, useRef } from 'react'
+import { motion } from "framer-motion"
+import emailjs from "@emailjs/browser"
+
+import { styles } from "../styles"
+import { EarthCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { slideIn } from "../utils/motion"
 
 const Contact = () => {
   return (
@@ -6,4 +13,4 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "")
