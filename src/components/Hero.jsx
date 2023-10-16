b162b7f4bae5b9f10e990374698bfc72efc7bfc9
KitwanaSh <rodriguekitweze@gmail.com>
2023-10-16 11:50:51 +0000

    Fix: Update padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputersCanvas } from './canvas'
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute top-[154px] inset-0 mx-w-7xl flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute top-[100px] inset-0 mx-w-7xl flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
